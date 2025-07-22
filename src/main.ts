a303f9a287154183031cb22ce429f9b1866bdd6d
Mateusz <mateusz.blach@interia.pl>
2025-07-22 23:08:12 +0200

    add Ionic components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { IonicModule } from '@ionic/angular';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(IonicModule.forRoot({
+      mode: 'md',
+      rippleEffect: false
+    })),
+    provideRouter(routes)
+  ]
+});
