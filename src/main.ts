c1034ec128da8483352892f6deac031a8f84f842
Mateusz <mateusz.blach@interia.pl>
2025-07-22 23:27:19 +0200

    refactor code

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
-import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { IonicModule } from '@ionic/angular';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(IonicModule.forRoot({
-      mode: 'md',
-      rippleEffect: false
-    })),
-    provideRouter(routes)
-  ]
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
