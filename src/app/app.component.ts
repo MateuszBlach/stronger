4eca0eb26de77cce5f1745ed6fd2757299dd2b08
Mateusz <mateusz.blach@interia.pl>
2025-07-27 16:23:23 +0200

    add tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import {DatabaseService} from './services/database.service';
 import {SplashScreen} from '@capacitor/splash-screen';
 import {IngredientsService} from './services/ingredients.service';
 import {IngredientComponent} from './components/ingredient/ingredient.component';
+import {IonicModule} from '@ionic/angular';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IngredientComponent],
+  imports: [RouterOutlet, IngredientComponent, IonicModule],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
