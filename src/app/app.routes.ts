f664b1997f2df8b10663d3082b6ddad5aa32ea90
Mateusz <mateusz.blach@interia.pl>
2025-07-29 20:14:43 +0200

    fix inresponsibility of ingredient component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: 'ingredient',
+        redirectTo: '/tabs/ingredient',
         pathMatch: 'full'
       }
     ]
