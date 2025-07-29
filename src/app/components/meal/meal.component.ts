8b06521c76c4c85f57717c582162b45984d1f0d4
Mateusz <mateusz.blach@interia.pl>
2025-07-29 23:09:49 +0200

    adding meals  (without ingredients)

--- src/app/components/meal/meal.component.ts
+++ src/app/components/meal/meal.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, WritableSignal} from '@angular/core';
+import {IonicModule} from '@ionic/angular';
+import {FormsModule} from '@angular/forms';
+import {MealService} from '../../services/meal.service';
+import {Meal} from '../../entity/meal';
 
 @Component({
   selector: 'app-meal',
-  imports: [],
+  imports: [IonicModule, FormsModule],
   templateUrl: './meal.component.html',
   standalone: true
 })
 export class MealComponent {
 
+  newMealName = ''
+
+  meals: WritableSignal<Meal[]>;
+
+  constructor(
+    private mealService: MealService,
+  ) {
+    this.meals = mealService.getMeals();
+  }
+
+
+  addNewMeal() {
+    this.mealService.addMeal(this.newMealName);
+    this.newMealName = '';
+  }
+
+  deleteMeal(id: number) {
+    this.mealService.deleteMeal(id.toString());
+  }
 }
